18b5fe7e4bf0d508a33038628f1dcb53f8bbb82f
kyle.buckley <kbuckle1@alumni.nd.edu>
2018-08-03 10:29:49 -0500

    Added basics for .NET Core 2 Web App to consume the data from RDS. Added Model, DBContext and simple querying function to pull test data from the RDS table.

---
+++ WebApp/WCIDemoWebApp/WCIDemoWebApp/Models/CoinData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WCIDemoWebApp.Models
+{
+    public class CoinData
+    {
+        private DBContext context;
+
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+    }
+}
---
+++ WebApp/WCIDemoWebApp/WCIDemoWebApp/Pages/CoinData.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WCIDemoWebApp.Models;
+
+namespace WCIDemoWebApp.Pages
+{
+    public class CoinDataModel : PageModel
+    {
+        public string CoinData { get; set; }
+        public void OnGet()
+        {
+            DBContext context = HttpContext.RequestServices.GetService(typeof(DBContext)) as DBContext;
+            CoinData = context.GetCoinData()[0].Text;
+
+        }
+    }
+}
